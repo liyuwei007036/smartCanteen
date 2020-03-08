7d8f76f3b004043527e87fd51b9dad9024f43e18
lc <599047945@qq.com>
2020-03-08 20:17:27 +0800

    订单

--- src/main/java/com/smart/canteen/entity/Order.java
+++ src/main/java/com/smart/canteen/entity/Order.java
@@ -1,19 +1,15 @@
 package com.smart.canteen.entity;
 
-import com.baomidou.mybatisplus.annotation.TableName;
-import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.*;
 import com.baomidou.mybatisplus.extension.activerecord.Model;
-import com.baomidou.mybatisplus.annotation.Version;
-import com.baomidou.mybatisplus.annotation.TableId;
-import java.time.LocalDateTime;
-import com.baomidou.mybatisplus.annotation.TableLogic;
-import com.baomidou.mybatisplus.annotation.TableField;
-import java.io.Serializable;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.experimental.Accessors;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
 
 /**
  * <p>
@@ -26,11 +22,11 @@ import lombok.experimental.Accessors;
 @Data
 @EqualsAndHashCode(callSuper = false)
 @Accessors(chain = true)
-@TableName("pay_log")
-@ApiModel(value="PayLog对象", description="充值记录")
+@TableName("order")
+@ApiModel(value = "order", description = "订单")
 public class Order extends Model<Order> {
 
-    private static final long serialVersionUID=1L;
+    private static final long serialVersionUID = 1L;
 
     @ApiModelProperty(value = "主键Id")
     @TableId(value = "id", type = IdType.AUTO)
